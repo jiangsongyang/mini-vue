53860b15cae1dfbcd3a1b5d3ae4bf299e3aade08
jiangsongyang <2744856707@qq.com>
2022-04-09 00:07:51 +0800

    [fix] : fix reactive option validate

--- src/reactivity/src/reactive.ts
+++ src/reactivity/src/reactive.ts
@@ -2,49 +2,49 @@ import {
   mutableHandler,
   readonlyHandlers,
   shallowReadonlyHandlers,
-} from './baseHandlers'
-import { isObj } from '../../shared'
+} from "./baseHandlers";
+import { isArr, isObj } from "../../shared";
 
 export interface Target {
-  [REACTIVE_FLAGS.IS_REACTIVE]?: boolean
-  [REACTIVE_FLAGS.IS_READONLY]?: boolean
+  [REACTIVE_FLAGS.IS_REACTIVE]?: boolean;
+  [REACTIVE_FLAGS.IS_READONLY]?: boolean;
 }
 
 export enum REACTIVE_FLAGS {
-  IS_REACTIVE = '__V_REACTIVE',
-  IS_READONLY = '__V_READONLY',
+  IS_REACTIVE = "__V_REACTIVE",
+  IS_READONLY = "__V_READONLY",
 }
 
 export function reactive<T extends Object>(raw: T) {
-  return createActiveObject(raw, mutableHandler)
+  return createActiveObject(raw, mutableHandler);
 }
 
 // readonly 只有 get
 export function readonly<T extends Object>(raw: T) {
-  return createActiveObject<T>(raw, readonlyHandlers)
+  return createActiveObject<T>(raw, readonlyHandlers);
 }
 
 function createActiveObject<T extends Object>(raw: T, baseHandlers: any) {
-  if (!isObj(raw)) {
-    console.warn(`target : ${raw} must be a object`)
+  if (typeof raw !== "object" && raw !== null) {
+    console.warn(`target : ${raw} must be a object`);
   }
-  return new Proxy(raw, baseHandlers)
+  return new Proxy(raw, baseHandlers);
 }
 
 export function shallowReadonly<T extends object>(raw: T) {
-  return new Proxy(raw, shallowReadonlyHandlers)
+  return new Proxy(raw, shallowReadonlyHandlers);
 }
 
 // isReactive 和 isReadOnly
 // 思路为 : 触发 getter 根据 handler 中的 isReadOnly 判断
 export function isReactive(value: unknown): boolean {
-  return !!(value as Target)[REACTIVE_FLAGS.IS_REACTIVE]
+  return !!(value as Target)[REACTIVE_FLAGS.IS_REACTIVE];
 }
 
 export function isReadOnly(value: unknown): boolean {
-  return !!(value as Target)[REACTIVE_FLAGS.IS_READONLY]
+  return !!(value as Target)[REACTIVE_FLAGS.IS_READONLY];
 }
 
 export function isProxy(value: unknown): boolean {
-  return isReadOnly(value) || isReactive(value)
+  return isReadOnly(value) || isReactive(value);
 }
